ce5ac785337213158e754be2c492bb9e1d0a54e4
Dragan Filipovic <vebkurs@gmail.com>
2014-08-04 02:18:06 +0200

    jQuery plugin for building DOM

--- js/rrssb.js
+++ js/rrssb.js
@@ -22,6 +22,85 @@
 	'use strict';
 
 
+	(function ( $ ) {
+	 
+	    $.fn.shareSocial = function( options ) {
+	 
+	        // Set default options
+	        var settings = $.extend({
+	            title: 'Title Sample',
+	            text: 'Text Sample',
+	            image: 'http://kurtnoble.com/labs/rrssb/media/rrssb-preview.png',
+	            url: window.location.href,
+	            baseUrl: 'http://kurtnoble.com',
+	            sharePopup: true,
+	            showText: true,
+	            shareText: '',
+	            shareIcon: true
+	        }, options );
+	        // Set social networks api links
+	 		var apiLinks = {
+	            twitter: 'https://twitter.com/intent/tweet?url={url}&text={text}',
+	            pinterest: 'https://www.pinterest.com/pin/create/button/?media={image}&url={url}&description={text}',
+	            facebook: 'https://www.facebook.com/sharer.php?s=100&p[title]={title}&p[summary]={text}&p[url]={url}&p[images][0]={image}',
+	            linkedin: 'https://www.linkedin.com/shareArticle?mini=true&url={url}&title={title}&summary={text}&source={baseUrl}',
+	            tumblr: 'https://tumblr.com/share?s=&v=3&t={title}&u={url}',
+	            plus: 'https://plus.google.com/share?url={url}',
+	            pocket: 'https://getpocket.com/edit?url={url}&title={title}'
+	        };
+	        // Update links with user settings data
+	        function apiLink(website){
+	            var apiUrl = apiLinks[website];
+	            apiUrl = apiUrl.replace('{url}', encodeURIComponent(settings.url));
+	            apiUrl = apiUrl.replace('{title}', encodeURIComponent(settings.title));
+	            apiUrl = apiUrl.replace('{text}', encodeURIComponent(settings.text));
+	            apiUrl = apiUrl.replace('{image}', encodeURIComponent(settings.image));
+	            apiUrl = apiUrl.replace('{baseUrl}', encodeURIComponent(settings.baseUrl));
+	            return apiUrl;
+	        };
+	        function buttonText(website) {
+	        	if(settings.showText) {
+	        		if (settings.shareText.length > 0) {
+			            var customText = settings.shareText.replace(settings.shareText, encodeURIComponent(settings.shareText));
+			            return '<span class="text">'+customText+'</span>';
+			        } else {
+				        return '<span class="text">'+website+'</span>';
+				    };
+	        	};
+			};
+			var icons = {
+
+			    linkedin: '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="28px" height="28px" viewBox="0 0 28 28" enable-background="new 0 0 28 28" xml:space="preserve"><path d="M25.424,15.887v8.447h-4.896v-7.882c0-1.979-0.709-3.331-2.48-3.331c-1.354,0-2.158,0.911-2.514,1.803 c-0.129,0.315-0.162,0.753-0.162,1.194v8.216h-4.899c0,0,0.066-13.349,0-14.731h4.899v2.088c-0.01,0.016-0.023,0.032-0.033,0.048 h0.033V11.69c0.65-1.002,1.812-2.435,4.414-2.435C23.008,9.254,25.424,11.361,25.424,15.887z M5.348,2.501 c-1.676,0-2.772,1.092-2.772,2.539c0,1.421,1.066,2.538,2.717,2.546h0.032c1.709,0,2.771-1.132,2.771-2.546 C8.054,3.593,7.019,2.501,5.343,2.501H5.348z M2.867,24.334h4.897V9.603H2.867V24.334z"/></svg>',
+
+			    facebook: '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="28px" height="28px" viewBox="0 0 28 28" enable-background="new 0 0 28 28" xml:space="preserve"><path d="M27.825,4.783c0-2.427-2.182-4.608-4.608-4.608H4.783c-2.422,0-4.608,2.182-4.608,4.608v18.434 c0,2.427,2.181,4.608,4.608,4.608H14V17.379h-3.379v-4.608H14v-1.795c0-3.089,2.335-5.885,5.192-5.885h3.718v4.608h-3.726 c-0.408,0-0.884,0.492-0.884,1.236v1.836h4.609v4.608h-4.609v10.446h4.916c2.422,0,4.608-2.188,4.608-4.608V4.783z"/></svg>',
+
+			    twitter: '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="28px" height="28px" viewBox="0 0 28 28" enable-background="new 0 0 28 28" xml:space="preserve"><path d="M24.253,8.756C24.689,17.08,18.297,24.182,9.97,24.62c-3.122,0.162-6.219-0.646-8.861-2.32 c2.703,0.179,5.376-0.648,7.508-2.321c-2.072-0.247-3.818-1.661-4.489-3.638c0.801,0.128,1.62,0.076,2.399-0.155 C4.045,15.72,2.215,13.6,2.115,11.077c0.688,0.275,1.426,0.407,2.168,0.386c-2.135-1.65-2.729-4.621-1.394-6.965 C5.575,7.816,9.54,9.84,13.803,10.071c-0.842-2.739,0.694-5.64,3.434-6.482c2.018-0.623,4.212,0.044,5.546,1.683 c1.186-0.213,2.318-0.662,3.329-1.317c-0.385,1.256-1.247,2.312-2.399,2.942c1.048-0.106,2.069-0.394,3.019-0.851 C26.275,7.229,25.39,8.196,24.253,8.756z"/></svg>'
+			};
+			
+	        this.each(function(index, el) {
+	        	$(el).addClass('rrssb-buttons');
+	        });
+	        this.children().each(function(index, el){
+	        	var website = $(el).attr('data-website');
+	        	if(settings.shareIcon) {
+				    var buttonIcon = '<span class="icon">'+icons[website]+'</span>';
+	        	} else {
+	        		return buttonIcon = '';
+	        	};
+	        	if (settings.sharePopup) {
+		            var hasPopup = 'popup';
+		        } else {
+		        	return hasPopup = '';
+		        };
+	        	console.log(website+'  ');
+	        	//$(el).addClass(website).append($('<a href="'+apiLink(website)+'">'+buttonIcon+buttonText(website)+'</a>').addClass(hasPopup));
+	        	$('<a href="'+apiLink(website)+'">'+buttonIcon+buttonText(website)+'</a>').addClass(hasPopup).appendTo($(el).addClass(website));
+
+	        });
+	    };    
+	 
+	}( jQuery ));
+
 	/*
 	 * Utility functions
 	 */
